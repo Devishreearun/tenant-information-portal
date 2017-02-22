cd4546ea9416cf6ba3d043751c030559ef1904d0
illakiyapalanisamy <illakiyapalanisamy@gmail.com>
2017-02-22 14:50:33 -0800

    portal

---
+++ testproj/app/Http/Controllers/AdminController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use DB;
+use Log;
+use App\Police;
+use App\Http\Requests;
+use Illuminate\Support\Facades\Input;
+use Redirect;
+class AdminController extends Controller
+{
+    public function check()
+  {  
+   }
+}
